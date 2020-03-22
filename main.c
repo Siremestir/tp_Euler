d9767ae2ead39403fd66e488ae30d4be3334d70d
Swan Sauvegrain <swan.sauvegrain@gmail.com>
2020-03-22 18:21:16 +0100

    fin

--- main.c
+++ main.c
@@ -4,6 +4,8 @@
 
 //1. Le type du dernier paramètre est une fonction de R dans R.
 
+// ============= PARTIE FAITE PAR ASTRID ===================
+
 float integrale_Rectangles(float a, float b, int nb_subdivisions, float f(float)){
     float somme = 0.0f;
     float h = (b-a) / ((float)nb_subdivisions);
@@ -36,6 +38,9 @@ float integrale_Trapezes(float a, float b, int nb_subdivisions, float f(float)){
 float g_exponentielle(float x, float y){
     return y;
 }
+
+// ===========================================================
+// Fin de la partie faite par Astrid
 
 //Fonction carré pour test
 float g_carre(float x, float y){
@@ -77,7 +82,7 @@ int methode_ordre_un_fichier(float x0, float y0, float xN, int nb_subdivisions,f
     }
 }
 
-//A quoi correspond le v ?
+//Si j'ai bien compris le v c'est y'
 float g_exponentielle_ordre2(float x, float y, float v){
     return y;
 }
@@ -92,6 +97,7 @@ float g_trigo(float x, float y, float v){
     return -y;
 }
 
+//Cette fonction sert à mes tests
 float methode_ordre_deux(float x0, float y0, float v0, float xN, int nb_subdivisions,float g(float, float, float)){
     float subdiv = (xN - x0)/(float)nb_subdivisions;
     float y_actuel = y0;
@@ -117,7 +123,6 @@ int methode_ordre_deux_fichier(float x0, float y0, float v0, float xN, int nb_su
     if(fichier!=NULL){
         for(int i=0; i<nb_subdivisions; i++){
             fprintf(fichier, "%f %f\n", x_actuel, y_actuel);
-            //Pas entièrement certain de l'ordre du v et du y ici
             y_actuel += subdiv*v_actuel;
             v_actuel += subdiv*g(x_actuel, y_actuel, v_actuel);
             x_actuel += subdiv;
@@ -137,6 +142,7 @@ int methode_ordre_deux_fichier(float x0, float y0, float v0, float xN, int nb_su
 //En partant de là on a fouillé dans les fonctions trigonométriques pour trouver une fonction qui correspond à tous les critères
 //f(x) = sin(x) + cos(x)
 //Reste à déterminer si la fonction methode_ordre_deux_fichier fait bien son boulot
+//Apparemment oui, elle fonctionne pour la fonction carre, et la courbe obtenue avec g_trigo correspond à la courbe f(x)=sin(x)+cos(x)
 
 int main(){
     printf("%f",integrale_Rectangles(0.0f, 1.0f, 100, fonction));
